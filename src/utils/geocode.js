173699423670611250293ef2cafe9bc045047198
Venu Madhav <madhav475@gmail.com>
2019-03-27 18:39:27 +0530

    Added data to about page

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ address + '.json?access_token=pk.eyJ1IjoibWFkaGF2NDc1IiwiYSI6ImNqdDl4OTZjeTA1ZGE0NHBjZ2d1YTFicmUifQ.zg-1Aq7RX--mwy9j-RnvLw&limit=1'
 
-    request({ url, json: true }, (error, {body}) => {
+    request({ url, json: true }, (error, { body }) => {
         if(error) {
             callback('unable to connect to location', undefined)
         } else if(body.features.length === 0){
